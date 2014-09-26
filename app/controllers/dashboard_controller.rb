88ba63c22c10cfc6df1856ae9b9fb8fe3be022bf
Iscenco <iscencoal@gmail.com>
2014-09-26 15:57:11 +0300

    https://app.asana.com/0/15730429473836/16693719049011. NEW: Added best and worst dealers to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
     @reports = Report.by_created_at("desc").limit(10)
     @best_shops = Shop.by_score("desc").limit(3)
     @worst_shops = Shop.by_score("asc").limit(3)
+    @best_dealers = Dealer.by_score("desc").limit(3)
+    @worst_dealers = Dealer.by_score("asc").limit(3)
   end
 
 
