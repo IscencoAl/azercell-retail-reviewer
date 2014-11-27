41c4366312dbc9f11357191dd31e6eeae11de2ba
Iscenco <iscencoal@gmail.com>
2014-11-27 15:20:24 +0200

    https://app.asana.com/0/15730429473836/21189266130347. On click change marker location on map.

--- app/assets/javascripts/maps/shop_edit.js
+++ app/assets/javascripts/maps/shop_edit.js
@@ -1,34 +1,33 @@
 function initialize() {
   var latitude = $("#shop_latitude").val(),
-    longitude = $("#shop_longitude").val(),
-    latlng = null,
-    mapOptions = {};
-  
+      longitude = $("#shop_longitude").val(),
+      latlng = null,
+      marker = null,
+      mapOptions = {};
+
   if(latitude != "" && longitude != ""){
     latlng = new google.maps.LatLng(latitude, longitude);
-    mapOptions = {
-      zoom: 14,
-      center: latlng
-    };
-  }else{
+    mapOptions = { zoom: 14, center: latlng };
+  } else {
     var center = new google.maps.LatLng(40.41349604970198, 47.647705078125);
-    mapOptions = {
-      zoom: 7,
-      center: center
-    };
+    mapOptions = { zoom: 7, center: center };
   }
   
   var map = new google.maps.Map($("#map_canvas")[0], mapOptions);
 
   if (latlng){
-    addMarker(latlng, map);
+    marker = addMarker(latlng, map);
   }
-  else{
-    google.maps.event.addListener(map, 'click', function(event) {
-      addMarker(event.latLng, map);
-      SetGeoloc(event.latLng)
-    });
-  }
+  
+  google.maps.event.addListener(map, 'click', function(event) {
+    if(marker == null){
+      marker = addMarker(event.latLng, map);
+    } else {
+      marker.setPosition(event.latLng);
+    }
+    
+    SetGeoloc(event.latLng);
+  });
 }
 
 function SetGeoloc(latlng){
@@ -36,13 +35,11 @@ function SetGeoloc(latlng){
   $("#shop_longitude").val(latlng.lng());
 }
 
-// Add a marker to the map
 function addMarker(location, map) {
   var marker = new google.maps.Marker({
     position: location,
     map: map,
-    draggable:true
-
+    draggable: true
   });
 
   google.maps.event.addListener(
@@ -52,8 +49,6 @@ function addMarker(location, map) {
       SetGeoloc(marker.position)
     }
   );
-
-  google.maps.event.clearListeners(map, 'click')
   
   return marker;
 }
