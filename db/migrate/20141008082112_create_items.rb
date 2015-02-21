691de2667127d3ce0780341134dd83a66e7839c6
Iscenco Alexandr <iscencoal@gmail.com>
2015-02-21 15:22:19 +0200

    Migration fix

--- db/migrate/20141008082112_create_items.rb
+++ db/migrate/20141008082112_create_items.rb
@@ -1,6 +1,6 @@
 class CreateItems < ActiveRecord::Migration
   def change
-    create_table :shop_items do |t|
+    create_table :items do |t|
       t.string :name
       t.boolean :is_deleted
     end
